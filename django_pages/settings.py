d9b0626b28049c3ff7da09686d5345d90a109fc8
Drl <ddufor1@lsu.edu>
2023-04-06 22:08:04 -0500

    heroku

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-of^wgbr^%a%4bwkhh#_=dp4(@8vbek5)7ab_+ao&3fy1o#77gj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
